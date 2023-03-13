931c64d906f8a6b78b23f7b024c8d519be12173c
Molly White <molly.white5@gmail.com>
2023-03-12 23:11:14 -0500

    Mobile-friendliness

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,4 +1,23 @@
+function toggleMenu() {
+  var ariaExpanded = this.getAttribute("aria-expanded");
+  var links = document.getElementById("nav-links");
+
+  links.classList.toggle("collapsed");
+
+  // Set aria attributes
+  if (ariaExpanded === "true") {
+    this.setAttribute("aria-expanded", false);
+    links.setAttribute("aria-hidden", true);
+  } else {
+    this.setAttribute("aria-expanded", true);
+    links.setAttribute("aria-hidden", false);
+  }
+}
+
 (function () {
   document.documentElement.className =
     document.documentElement.className.replace(/\bno-js\b/, "js");
+
+  var menuButton = document.getElementById("menu-toggle");
+  menuButton.addEventListener("click", toggleMenu);
 })();
