41fabd7956878635ce966d5935aa256437a2b984
Molly White <molly.white5@gmail.com>
2023-11-22 17:03:31 -0500

    Update verification

---
+++ src/_data/w3iggSocialLinks.js
@@ -0,0 +1,29 @@
+const data = [
+  {
+    label: "Twitter",
+    href: "https://twitter.com/web3isgreat",
+    username: "web3isgreat",
+  },
+  {
+    label: "Mastodon",
+    href: "https://indieweb.social/@web3isgreat",
+    username: "@web3isgreat@indieweb.social",
+  },
+  {
+    label: "Bluesky",
+    href: "https://bsky.app/profile/did:plc:ugyl6syayvsrvu5w4uxtlkz4",
+    username: "@web3isgoinggreat.com",
+  },
+  {
+    label: "Instagram",
+    href: "https://www.instagram.com/web3isgoinggreat/",
+    username: "web3isgoinggreat",
+  },
+  {
+    label: "Threads",
+    href: "https://www.threads.net/@web3isgoinggreat",
+    username: "web3isgoinggreat",
+  },
+];
+
+module.exports = data;
