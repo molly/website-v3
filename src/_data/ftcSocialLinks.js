62ad43d566b7c0af766c1f260fd689da5a6c20eb
Molly White <molly.white5@gmail.com>
2024-07-28 10:20:16 -0400

    + Follow the Crypto links

---
+++ src/_data/ftcSocialLinks.js
@@ -0,0 +1,19 @@
+const data = [
+  {
+    label: "Twitter",
+    href: "https://twitter.com/follow__crypto",
+    username: "follow__crypto",
+  },
+  {
+    label: "Mastodon",
+    href: "https://hachyderm.io/followthecrypto",
+    username: "@followthecrypto@hachyderm.io",
+  },
+  {
+    label: "Bluesky",
+    href: "https://bsky.app/profile/did:plc:spedjz5pnontltd5cd74jdf3",
+    username: "@followthecrypto.org",
+  },
+];
+
+module.exports = data;
